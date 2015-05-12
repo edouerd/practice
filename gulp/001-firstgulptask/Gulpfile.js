d9575ffe68adeb32488fcd56e809bded445b99d1
édouard u <mail@edouard.us>
2015-05-11 23:33:38 -0500

    + gulp shit that doesn't work :<

--- gulp/001-firstgulptask/Gulpfile.js
+++ gulp/001-firstgulptask/Gulpfile.js
@@ -1,5 +1,16 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant');
 
-gulp.task('default', function() {
+// Change default task to use compress_images task
+gulp.task('default', ['compress_images']);
 
+// Compress png images from images/ folder
+gulp.task('compress_images', function () {
+       	return gulp.src('images/**')
+		.pipe(imagemin({
+			progressive: true,
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest('_site/images'));
 });
